61f160361d5b6cfff93037ee02a16bd5cf79563d
mitsu-h <u.world96@gmail.com>
2023-04-02 20:41:32 +0900

    typescriptに変更

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,33 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string
+}>()
+</script>
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <button @click="fetchData">Fetch Data from Django</button>
-    <p>{{ apiData.message }}</p>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+    </h3>
   </div>
 </template>
 
-<script>
-import axios from "axios";
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
 
-export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
-  },
-  data() {
-    return {
-      apiData: {},
-    };
-  },
-  methods: {
-    async fetchData() {
-      try {
-        const response = await axios.get("http://localhost:8081/api/hello/");
-        this.apiData = response.data;
-    } catch (error) {
-      console.error('API request error:', error);
-    }
-  },
+h3 {
+  font-size: 1.2rem;
 }
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
 }
-</script>
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
