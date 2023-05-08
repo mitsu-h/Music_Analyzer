8b587312d36c6f4400f190b77213a06154593e2b
mitsu-h <u.world96@gmail.com>
2023-05-09 08:44:46 +0900

    時間のスキップにバグあるけどとりあえず再生位置の調整実装

--- frontend/src/views/Analysis.vue
+++ frontend/src/views/Analysis.vue
@@ -4,18 +4,17 @@
     <v-main>
       <h1>Analysis</h1>
       <v-container fluid>
-        <v-tooltip top>
-    <template v-slot:activator="{ on }">
-      <v-progress-linear
-        v-on="on"
-        v-model="progress"
-        :value="progress"
-        @click="seek"
-        height="20"
-      ></v-progress-linear>
-    </template>
-    <span>{{ currentTime }} / {{ duration }}</span>
-  </v-tooltip>
+          <v-slider
+           v-bind:model-value="playbackPosition"
+            @click="controllPlayback"
+            label="Playback Position"
+            step="0.1"
+            min="0"
+            :max="duration"
+            thumb-label="always"
+            thumb-size="24"
+          ></v-slider>
+    <span>{{ currentTime }} / {{ durationTime }}</span>
         <v-btn @click="togglePlay">再生/停止</v-btn>
       <v-row>
           <v-col v-for="(track, index) in trackLabels" :key="index" cols="12" sm="6" md="4">
@@ -55,7 +54,9 @@ export default defineComponent({
     const decodedAudioDataList = ref(null);
     const mixedAudioBuffer = ref(null);
     const playStartTime = ref(null);
-    const sourceNode = ref(null);
+    const deltaTime = ref(0);
+    const duration = ref(null);
+    const durationTime = computed(() => formatTime(duration.value || 0));
     const gainNodes = ref([]);
     
     // トラックラベルとゲインのリアクティブ変数を定義
@@ -64,13 +65,15 @@ export default defineComponent({
 
     function startTimer() {
         if (playStartTime.value !== null) {
+          //  console.log(audioContext.currentTime);
             const elapsedTime = audioContext.currentTime - playStartTime.value;
             const duration = gainNodes.value[0]?.source?.buffer?.duration;
-            currentTime.value = formatTime(elapsedTime % duration);
+            playbackPosition.value = (elapsedTime / duration) * 100  + deltaTime.value;
+            currentTime.value = formatTime(elapsedTime % duration  + deltaTime.value);
           }else currentTime.value = formatTime(0);
           setTimeout(() => {
             startTimer();
-          }, 1000);
+          }, 100);
       }
       
 
@@ -108,10 +111,10 @@ export default defineComponent({
         console.log(gainNodes.value)
         mixedAudioBuffer.value = mixAudioBuffers(decodedAudioDataList.value, audioContext, gainNodes.value);
         playAudioBuffer(audioContext, gainNodes.value);
-        playStartTime.value = audioContext.currentTime
+        playStartTime.value = audioContext.currentTime;
+        duration.value = mixedAudioBuffer.value.duration;
         startTimer()
 
-        // audioElement.value.addEventListener("timeupdate", updateCurrentTime);
       } catch (error) {
         console.error("Error fetching audio:", error);
       }
@@ -120,22 +123,38 @@ export default defineComponent({
     const progress = ref(0);
     const currentTime = ref(0);
 
-    const duration = computed(() => formatTime(mixedAudioBuffer.value?.duration || 0));
+    const playbackPosition = ref(0);
 
-    watch(
-      () => audioElement.value?.currentTime,
-      (value) => {
-        progress.value = (value / audioElement.value.duration) * 100;
-      },
-      { deep: true }
-    );
+    function controllPlayback (event) {
+      // クリック位置を0～1の範囲に正規化
+      const clickPosition = event.offsetX / event.target.clientWidth;
+
+      // 再生位置を計算
+      const newPlaybackPosition = clickPosition * duration.value;
+
+      gainNodes.value.forEach((gainNode) => {
+        if (gainNode.source) {
+          gainNode.source.stop();
+          gainNode.source.disconnect();
+        }
+        const source = audioContext.createBufferSource();
+        source.buffer = gainNode.source.buffer;
+        source.loop = true;
+        source.connect(gainNode.gainNode);
+        source.start(0, newPlaybackPosition);
+        gainNode.source = source;
+        });
+        deltaTime.value = newPlaybackPosition - playbackPosition.value
+      }
+      
+
 
     function seek(event) {
       const rect = event.target.getBoundingClientRect();
       const x = event.clientX - rect.left;
-      const seekTime = (x / rect.width) * audioElement.value.duration;
-      audioElement.value.currentTime = seekTime;
-      progress.value = (seekTime / audioElement.value.duration) * 100;
+      const seekTime = (x / rect.width) * mixedAudioBuffer.value?.duration;
+      audioContext.currentTime = seekTime;
+      progress.value = (seekTime / audioContext.duration) * 100;
     }
 
     function formatTime(time) {
@@ -159,11 +178,15 @@ export default defineComponent({
       mixedAudioBuffer,
       trackLabels,
       gains,
+      playbackPosition,
       progress,
       currentTime,
       duration,
+      durationTime,
       seek,
-      togglePlay
+      togglePlay,
+      formatTime,
+      controllPlayback
     };
   },
 });
